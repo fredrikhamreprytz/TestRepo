3d49d5191dfcd4a28eeca2585a137d1dcccc40fc
h181204 <fprytz@gmail.com>
2018-03-21 14:06:26 +0100

    * HelloiOS.csproj:
    * NewClass.cs: Ny klasse

---
+++ NewClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace HelloiOS
+{
+    public class NewClass
+    {
+        private int number;
+        public NewClass()
+        {
+            number = 1;
+        }
+    }
+}
