32cfc79177945a6c0623a101d8e97db518f5d977
h181204 <fprytz@gmail.com>
2018-03-21 14:14:00 +0100

    Test commit

--- NewClass.cs
+++ NewClass.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 namespace HelloiOS
 {
+    //Testklasse
     public class NewClass
     {
         private int number;
+        private string streng;
         public NewClass()
         {
             number = 1;
+
         }
     }
 }
